1ff98f323376833ad7bf91176589998520eb588e
BugHunter-X <askzenai@gmail.com>
2025-01-18 11:40:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,21 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Incorrect logic here
+    if (count > 5) {
+      console.log('Count is greater than 5');
+    } else {
+      console.log('Count is not greater than 5');
+    }
+  }, [count]);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+```
